81f7f334478ec993a2168166f7ed64ec9ce06a15
Christophe Bral <30625064+Braloup@users.noreply.github.com>
2020-10-04 16:14:02 +0200

    Capitre 2 balise sript + defer  + commentaire

---
+++ Chapitre 2/02_02/alert.js
@@ -0,0 +1,4 @@
+console.log("Bonjour de javascript!");
+console.info("Au revoir de javascript!");
+console.warn("Bonsoir de javascript!");
+console.error("Bonne nuit de javascript!");
---
+++ Chapitre 2/02_07/simple.js
@@ -0,0 +1,3 @@
+// Aller chercher l'élément h1 portant l'identifiant "heading" dans la page.
+var heading = document.getElementById("heading");
+heading.innerHTML = "Une simple page HTML"; //Mettre du contenu dans cet élément h1
